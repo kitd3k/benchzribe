684e76ca7fdfd495c4d1ab6d14c5a281a223312c
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 16:55:37 +0000

    add Go Benchmark (go) benchmark result for bf7dd285ffa67f347e873d0b6161dea554a8444f

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1745340937968,
+  "repoUrl": "https://github.com/kitd3k/benchzribe",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tijo1293@gmail.com",
+            "name": "Tijo Thomas",
+            "username": "tijo-thomaz"
+          },
+          "committer": {
+            "email": "tijo1293@gmail.com",
+            "name": "Tijo Thomas",
+            "username": "tijo-thomaz"
+          },
+          "distinct": true,
+          "id": "bf7dd285ffa67f347e873d0b6161dea554a8444f",
+          "message": "fix: improve workflow configuration\n\n- Add proper gh-pages branch handling\n\n- Update action versions\n\n- Improve workflow structure",
+          "timestamp": "2025-04-22T17:54:51+01:00",
+          "tree_id": "1d1d8265f9ed02972b4c641272f6981e945d5bed",
+          "url": "https://github.com/kitd3k/benchzribe/commit/bf7dd285ffa67f347e873d0b6161dea554a8444f"
+        },
+        "date": 1745340937329,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkTestHandler",
+            "value": 2072,
+            "unit": "ns/op\t    6132 B/op\t      19 allocs/op",
+            "extra": "561926 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - ns/op",
+            "value": 2072,
+            "unit": "ns/op",
+            "extra": "561926 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - B/op",
+            "value": 6132,
+            "unit": "B/op",
+            "extra": "561926 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - allocs/op",
+            "value": 19,
+            "unit": "allocs/op",
+            "extra": "561926 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
