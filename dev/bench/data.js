68ecffdad833d7686cead667c59402abef4f6f72
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 16:57:23 +0000

    add Go Benchmark (go) benchmark result for 674a7f637b0165cf1c6386c67bca2f9234595fbe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745340937968,
+  "lastUpdate": 1745341043325,
   "repoUrl": "https://github.com/kitd3k/benchzribe",
   "entries": {
     "Go Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 19,
             "unit": "allocs/op",
             "extra": "561926 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tijo1293@gmail.com",
+            "name": "Tijo Thomas",
+            "username": "tijo-thomaz"
+          },
+          "committer": {
+            "email": "tijo1293@gmail.com",
+            "name": "Tijo Thomas",
+            "username": "tijo-thomaz"
+          },
+          "distinct": true,
+          "id": "674a7f637b0165cf1c6386c67bca2f9234595fbe",
+          "message": "merge: resolve conflicts with main\n\n- Keep latest workflow improvements\n\n- Maintain graph functionality",
+          "timestamp": "2025-04-22T17:56:21+01:00",
+          "tree_id": "43c6d312190611f6b9d9180eebc605d0ac07d725",
+          "url": "https://github.com/kitd3k/benchzribe/commit/674a7f637b0165cf1c6386c67bca2f9234595fbe"
+        },
+        "date": 1745341042692,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkTestHandler",
+            "value": 1968,
+            "unit": "ns/op\t    5747 B/op\t      18 allocs/op",
+            "extra": "615904 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - ns/op",
+            "value": 1968,
+            "unit": "ns/op",
+            "extra": "615904 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - B/op",
+            "value": 5747,
+            "unit": "B/op",
+            "extra": "615904 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTestHandler - allocs/op",
+            "value": 18,
+            "unit": "allocs/op",
+            "extra": "615904 times\n4 procs"
           }
         ]
       }
